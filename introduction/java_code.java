b639a0d4f8dc24cc0ae7632a4aef5a740137e2ca
checkfree\yjiang <yang.jiang@fiserv.com>
2019-03-01 11:35:43 +1300

    na

---
+++ introduction/java_code.java
@@ -0,0 +1,12 @@
+package com.fiserv.accounts.api;
+
+import com.fiserv.coremodule.mobilitirestclient.MobilitiCachingResponseHandler;
+
+public class AccountsResponseHandler extends MobilitiCachingResponseHandler {
+
+    String a = "a";
+    String b = "b";
+    String c = "c";
+    String d = "d";
+    String e = "e";
+}
