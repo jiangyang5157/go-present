94b6a047c1d09675d819b58ae9d83b6c44741275
yangjiang <jiangyang5157@gmail.com>
2019-03-12 22:00:33 +1300

    na

---
+++ introduction/main.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+    // START_1 OMIT
+    static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World - HLthisline"); // HLthisline
+    }
+    // STOP OMIT
+
+    // START_2 OMIT
+    int addmain(int a, int b) {
+        return a + b;
+    }
+    // STOP OMIT
+}
